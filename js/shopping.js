0c874908375be93fbd450e73f508fd57cdeae280
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-18 16:30:28 +0000

    storing all carts items in an array&/object

--- js/shopping.js
+++ js/shopping.js
@@ -1,15 +1,23 @@
+// for storing all the cart items
+const cartArray = [];
+
 function addToCart(element) {
   //   console.log("addToCart", element);
   //   console.log(element.parentNode.parentNode.children);
-
   //   console.log(element.parentNode.parentNode.children[0].innerText);
-
   //   console.log(element.parentNode.parentNode.children[1].children[0].innerText);
-
+  //   getting the product name
   const productName = element.parentNode.parentNode.children[0].innerText;
-  console.log(productName);
-
+  //   console.log(productName);
+  //   getting the product price
   const productPrice =
     element.parentNode.parentNode.children[1].children[0].innerText;
-  console.log(productPrice);
+  //   console.log(productPrice);
+  //   creating a new object for the cart item
+  const productObj = {
+    productName: productName,
+    // productPrice: productPrice,
+    productPrice: parseFloat(productPrice),
+  };
+  console.log(productObj);
 }
