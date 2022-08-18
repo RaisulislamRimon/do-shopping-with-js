4ed04258d9d9b70b28e9789edc7b6d5f2e31a3d9
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-18 16:20:23 +0000

    update

--- js/shopping.js
+++ js/shopping.js
@@ -1,3 +1,7 @@
-function addToCart(x) {
-  console.log("addToCart", x);
+function addToCart(element) {
+  //   console.log("addToCart", element);
+  //   console.log(element.parentNode.parentNode.children);
+
+  console.log(element.parentNode.parentNode.children[0].innerText);
+  console.log(element.parentNode.parentNode.children[1]);
 }
