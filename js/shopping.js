751073b9e9a2eedc9755c8eb7a5078286f771b9e
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-18 16:25:36 +0000

    getting the product name & product price

--- js/shopping.js
+++ js/shopping.js
@@ -2,6 +2,14 @@ function addToCart(element) {
   //   console.log("addToCart", element);
   //   console.log(element.parentNode.parentNode.children);
 
-  console.log(element.parentNode.parentNode.children[0].innerText);
-  console.log(element.parentNode.parentNode.children[1]);
+  //   console.log(element.parentNode.parentNode.children[0].innerText);
+
+  //   console.log(element.parentNode.parentNode.children[1].children[0].innerText);
+
+  const productName = element.parentNode.parentNode.children[0].innerText;
+  console.log(productName);
+
+  const productPrice =
+    element.parentNode.parentNode.children[1].children[0].innerText;
+  console.log(productPrice);
 }
